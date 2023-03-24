9edf2df293de7c060b5b2dfa9cf7792e3c98d162
Ishanpathak1 <88367734+Ishanpathak1@users.noreply.github.com>
2023-03-24 09:10:10 +0530

    add:frontend,changed:nyt->tmdb

--- app.py
+++ app.py
@@ -1,22 +1,41 @@
+from flask import Flask, request, render_template
 import requests
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
-# Make a request to the NYTimes API to get movie reviews
-api_key = "5q6Hxy62fAysqZQlMX8DazIoAqJG7lnR"
-url = f"https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=godfather&api-key={api_key}"
-response = requests.get(url)
-data = response.json()
 
-# Get the reviews and perform sentiment analysis on each review
-sid = SentimentIntensityAnalyzer()
-for review in data["results"]:
-    review_text = review["summary_short"]
-    scores = sid.polarity_scores(review_text)
-    
-    # Print the sentiment scores for each review
-    print(f"Review: {review_text}")
-    print(f"Positive sentiment: {scores['pos']}")
-    print(f"Negative sentiment: {scores['neg']}")
-    print(f"Neutral sentiment: {scores['neu']}")
-    print(f"Overall sentiment score: {scores['compound']}")
-    print("--------------------------------------------------")
+app = Flask(__name__)
+api_key = "e0a0015ede8a824fab642d5d1660255e"
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        movie_name = request.form['movie_name']
+
+        # Make a request to the TMDb API to search for the movie by name
+        search_url = f"https://api.themoviedb.org/3/search/movie?api_key={api_key}&query={movie_name}"
+        search_response = requests.get(search_url)
+        search_results = search_response.json()
+
+        # Get the ID of the first result from the search
+        movie_id = search_results["results"][0]["id"]
+
+        # Make a request to the TMDb API to get movie details and reviews
+        movie_url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}&append_to_response=reviews"
+        movie_response = requests.get(movie_url)
+        movie_data = movie_response.json()
+
+        # Extract the reviews from the movie data
+        reviews = []
+        for review in movie_data["reviews"]["results"]:
+            review_text = review["content"]
+            reviews.append(review_text)
+
+        return render_template('reviews.html', movie_name=movie_name, reviews=reviews)
+    else:
+        return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
